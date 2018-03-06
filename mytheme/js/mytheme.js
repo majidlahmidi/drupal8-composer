d47dbc9a61ef475e4f2e8b113d6eeaee015b3a5b
Majid LAHMIDI <alahmidi@ausy-group.com>
2018-03-06 16:15:54 +0000

    first commit

---
+++ mytheme/js/mytheme.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * mytheme behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.mytheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
